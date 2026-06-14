250181bbd9b86f47ad86f7024a92dbca05648a54
Nevoths Bezos <danhc7620@gmail.com>
2026-06-14 22:14:30 +0700

    cn1
    done

--- miniproject.py
+++ miniproject.py
@@ -1,3 +1,38 @@
+import logging
+
+
+logging.basicConfig(
+    filename = "check.log",
+    filemode = "a",
+    level = logging.DEBUG,
+    format = '%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+devices = [
+    {'id': 'M01', 'location': 'Mechanical Shop A', 'old_index': 1200, 'new_index': 4500, 'status': 'Normal'},
+    {'id': 'M02', 'location': 'Assembly Line B', 'old_index': 2300, 'new_index': 8500, 'status': 'Overload'}
+]
+
+
+def show_devices(devices):
+    logging.info("Nguoi dung yeu cau xem danh sach thiet bi.")
+
+    if len(devices) == 0:
+        logging.warning("Danh sach thiet bi dang trong, khong co du lieu de hien thi.")
+        print("He thong hien chua co thiet bi giam sat nao!")
+        return
+ 
+    print("--- Danh Sách Thiết Bị Giám Sát ---")
+    print(f"{'MA TB':<8} | {'VI TRI PHAN XUONG':<22} | {'CHI SO CU':>10} | {'CHI SO MOI':>10} | {'TRANG THAI'}")
+    print("-" * 70)
+ 
+    for item in devices:
+        print(f"{item['id']:<8} | {item['location']:<22} | {item['old_index']:>10} | {item['new_index']:>10} | {item['status']}")
+
+    logging.info(f"Hien thi thanh cong {len(devices)} thiet bi.")
+
+
 def show_menu():
     print("\n" + "="*50)
     print("      SMART ENERGY MONITOR - PHÒNG CƠ ĐIỆN      ")
@@ -9,12 +44,8 @@ def show_menu():
     print("5. Thoát chương trình")
     print("="*50)
 
+
 def main():
-    devices = [
-        {'id': 'M01', 'location': 'Mechanical Shop A', 'old_index': 1200, 'new_index': 4500, 'status': 'Normal'},
-        {'id': 'M02', 'location': 'Assembly Line B', 'old_index': 2300, 'new_index': 8500, 'status': 'Overload'}
-    ]
-    
     while True:
         show_menu()
         
@@ -26,6 +57,7 @@ def main():
             
         if choice == 1:
             print("\n--> Bạn đã chọn Chức năng 1: Xem danh sách thiết bị.")
+            show_devices(devices)
             
         elif choice == 2:
             print("\n--> Bạn đã chọn Chức năng 2: Cập nhật chỉ số điện tiêu thụ.")
